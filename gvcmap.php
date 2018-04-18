b799cf15ab4384c5eb811a7d45ca59dc262e05f4
gio <giorodriguez021@gmail.com>
2018-04-18 23:49:30 +0800

    Deployment version

--- gvcmap.php
+++
@@ -1,3 +0,0 @@
-<div class="map">
-	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3860.8269473131495!2d121.09540931439669!3d14.608931989797158!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397b813f67a35c9%3A0x125f2351c6729cfe!2sGreenville+College!5e0!3m2!1sen!2sph!4v1514479755077" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
-</div>
